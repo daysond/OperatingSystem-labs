8143d5ef42be4790b98707453211f19589037de9
daysond <daysond@outlook.com>
2023-02-19 21:11:08 -0500

    server clients now able to communicate

--- lab5/server.cpp
+++ lab5/server.cpp
@@ -4,6 +4,7 @@
 //
 
 #include <arpa/inet.h>
+#include <fcntl.h> //  for nonblocking
 #include <iostream>
 #include <net/if.h>
 #include <netinet/in.h>
@@ -14,7 +15,6 @@
 #include <sys/types.h>
 #include <sys/un.h>
 #include <unistd.h>
-
 using namespace std;
 
 #define DEBUG
@@ -23,16 +23,17 @@ const char LOCALHOST[] = "127.0.0.1";
 bool isRunning = true;
 const int BUF_LEN = 4096;
 const int NUMCLIENT = 3;
-typedef struct message {
-    int len;
-    char buf[BUF_LEN];
-} Message;
+// typedef struct message {
+//     int len;
+//     char buf[BUF_LEN];
+// } Message;
 
-queue<Message> messageQueue;
+queue<string> messageQueue;
 pthread_mutex_t lock_x;
 
 int setupAddr(struct sockaddr_in &addr, int &sock, const char *port);
 void errExit(string, int &);
+void *recv_func(void *arg);
 
 int main(int argc, char const *argv[]) {
 
@@ -45,14 +46,23 @@ int main(int argc, char const *argv[]) {
     int ret;
     int connections = 0;
     int clients[NUMCLIENT];
-    sockaddr_in cl_addrs[NUMCLIENT];
     char buf[BUF_LEN];
+    sockaddr_in cl_addrs[NUMCLIENT];
     struct sockaddr_in addr;
+    pthread_t tids[NUMCLIENT];
 
+    memset(&clients, 0, sizeof(clients));
+
+    cout << clients[0] << clients[1] << clients[2] << endl;
     //  create a socket
     int master_socket = socket(AF_INET, SOCK_STREAM, 0);
-    int flags = fcntl(master_socket, F_GETFL, 0);
-    fcntl(master_socket, F_SETFL, flags | O_NONBLOCK);
+
+    // set socket timeout / non-blocking
+    // struct timeval tv;
+    // tv.tv_sec = 5;
+    // tv.tv_usec = 0;
+    // setsockopt(master_socket, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv,
+    //            sizeof(tv));
 
     //  setup addr (hint)
     ret = setupAddr(addr, master_socket, argv[1]);
@@ -67,26 +77,53 @@ int main(int argc, char const *argv[]) {
     if (listen(master_socket, 5) == -1)
         errExit("Server: cannot listen", master_socket);
 
+    pthread_mutex_init(&lock_x, NULL);
+
     while (isRunning) {
-        while (connections < 3)
-        {   
-            #if defined(DEBUG)
-            cout << "accept()" << endl;
-            #endif
-
+        if (connections < 3) {
             socklen_t cl_size = sizeof(cl_addrs[connections]);
-            if ( (clients[connections] = accept(master_socket, (sockaddr* ) &cl_addrs[connections],  &cl_size)) == -1) 
-                errExit("Server: cannot accept client", master_socket);
-            else
-                connections += 1;
-
+            ret = clients[connections] = accept(
+                master_socket, (sockaddr *)&cl_addrs[connections], &cl_size);
             #if defined(DEBUG)
-            cout << "accepting..." << endl;
+            cout << "accept()ed client " << clients[connections] << endl;
             #endif
-        }
-        
+            if (ret == -1) {
+                errExit("Server: cannot accept client", master_socket);
+            } else {
+// once connect , start recv thread
+// create thread
+#if defined(DEBUG)
+                cout << "creating thread () client: " << clients[connections]
+                     << endl;
+#endif
+                ret = pthread_create(&tids[connections], NULL, recv_func,
+                                     &clients[connections]);
+                if (ret != 0)
+                    errExit("Cannot create receive thread", master_socket);
+                connections += 1;
+            }
 
+            // main print message then pop
+            // Print out to the screen anything in the message queue. Be sure to
+            // mutex this message queue since it is used in the receive thread
+            // as well.
+
+            // Sleep for one second.
+        }
+
+        if (messageQueue.empty() == 0) {
+            pthread_mutex_lock(&lock_x);
+            cout << "######" << messageQueue.front() << endl;
+            messageQueue.pop();
+            pthread_mutex_unlock(&lock_x);
+        }
+
+        sleep(1);
     }
+
+    //  join threads here
+
+    // clean up...
 
     return 0;
 }
@@ -106,4 +143,32 @@ void errExit(string err, int &sock) {
     cout << strerror(errno) << endl;
     close(sock);
     exit(-1);
+}
+
+void *recv_func(void *arg) {
+    int client_fd = *(int *)arg;
+    char buf[BUF_LEN];
+
+#if defined(DEBUG)
+    cout << "read() client: " << client_fd << endl;
+#endif
+    
+    struct timeval tv;
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+    setsockopt(client_fd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv,
+               sizeof(tv));
+
+    while (isRunning) {
+        if (read(client_fd, buf, BUF_LEN) > 0) {
+            pthread_mutex_lock(&lock_x);
+            messageQueue.push(buf);
+            // messageQueue.push(to_string(client_fd));
+            pthread_mutex_unlock(&lock_x);
+        } else {
+            cout << "err handling..." << endl;
+        }
+    }
+
+    pthread_exit(NULL);
 }
